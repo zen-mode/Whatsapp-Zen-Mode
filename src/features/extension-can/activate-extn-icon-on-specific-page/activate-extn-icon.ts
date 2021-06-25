92f0a2d81f6212f80a8b3c1486e11df2572857b0
no_link_to <no.link.to@gmail.com>
2021-06-25 18:25:37 +0300

    feature: whatsapp tab reload

--- src/features/extension-can/activate-extn-icon-on-specific-page/activate-extn-icon.ts
+++ src/features/extension-can/activate-extn-icon-on-specific-page/activate-extn-icon.ts
@@ -7,6 +7,14 @@ import {browser} from "webextension-polyfill-ts";
 
 // 1. Makes extn icon active on https://web.whatsapp.com/; disabled otherwise.
 browser.runtime.onInstalled.addListener(function () {
+  // Reload WhatsApp tab
+  browser.tabs.query({}).then((tabs: any) => {
+    if (tabs && tabs.length) {
+      tabs.map((tab: any) => {
+        if (tab.url.includes("web.whatsapp.com"))  browser.tabs.reload(tab.id);
+      })
+    }
+  });
   browser.declarativeContent.onPageChanged.removeRules(undefined, function () {
     browser.declarativeContent.onPageChanged.addRules([
       {
