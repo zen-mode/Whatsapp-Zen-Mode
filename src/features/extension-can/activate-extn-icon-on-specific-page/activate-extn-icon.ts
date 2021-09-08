092da11d52f4214e1177fe168882ff3cb5405a38
Anatoliy Tikhonov <whiter@inbox.ru>
2021-09-08 17:54:57 +0500

    replaces welcome page tab with notification toast

--- src/features/extension-can/activate-extn-icon-on-specific-page/activate-extn-icon.ts
+++ src/features/extension-can/activate-extn-icon-on-specific-page/activate-extn-icon.ts
@@ -3,17 +3,35 @@
 // Description:
 // 1. Makes extn icon active on https://web.whatsapp.com/; disabled otherwise.
 
-import {browser} from "webextension-polyfill-ts";
+import {browser, Notifications} from "webextension-polyfill-ts";
 
 // 1. Makes extn icon active on https://web.whatsapp.com/; disabled otherwise.
 
 const reloadReasons = new Set(["install", "update"])
-browser.runtime.onInstalled.addListener(function ({reason}) {
-  // Reload WhatsApp tab
+browser.runtime.onInstalled.addListener(async function ({reason}) {
   if (reloadReasons.has(reason)) {
-    browser.tabs.create({
-      url: 'onboarding.html'
-    });
+    const level = await ((
+      browser.notifications as any
+    ).getPermissionLevel() as Promise<Notifications.PermissionLevel>);
+    const manifest = browser.runtime.getManifest();
+    const name = manifest.name;
+    const version = manifest.version;
+    Notification.requestPermission();
+    if (level === "granted") {
+      browser.notifications.create({
+        title: `${name} ${version}`,
+        message: browser.i18n.getMessage("ZM_update_notification_message"),
+        iconUrl: (browser.extension as any).getURL("assets/logo/logo.png"),
+        type: "basic",
+        buttons: [
+          { title: `${String.fromCharCode(0x200F)}${browser.i18n.getMessage("ZM_update_notification_ok_button_label")}` }
+        ],
+      } as Notifications.CreateNotificationOptions);
+    } else {
+      browser.tabs.create({
+        url: "onboarding.html",
+      });
+    }
   }
   browser.declarativeContent.onPageChanged.removeRules(undefined, function () {
     browser.declarativeContent.onPageChanged.addRules([
