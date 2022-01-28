f1ed0ae8e80106c9cc4f17bc7ec90d4afe60d834
Anatoliy Tikhonov <whiter@inbox.ru>
2022-01-28 09:03:39 +0500

    code cleanup

--- src/whatsapp/WWAProvider.ts
+++ src/whatsapp/WWAProvider.ts
@@ -13,7 +13,6 @@ export let ConnModule: any = null;
 
 export function provideModules(): void {
   const moduleRaid = moduleRaidV5();
-  (window as any).moduleRaid = moduleRaid;
   WapModule = moduleRaid.findModule('queryLinkPreview')[0].default;
   ChatModule = moduleRaid.findModule('Chat')[2].default;
   CmdModule = moduleRaid.findModule('Cmd')[0].Cmd;
