1522114b0515308881e7ee6c39367f290175a81b
Anatoliy Tikhonov <whiter@inbox.ru>
2022-02-26 15:57:19 +0500

    fix hidden chats drawer layout

--- src/whatsapp/ui/LeftDrawerHiddenChats.ts
+++ src/whatsapp/ui/LeftDrawerHiddenChats.ts
@@ -123,8 +123,8 @@ function constructBasicChatListElement(chat: Chat): HTMLElement {
 
 function constructEmptyPlug(): HTMLElement {
   document.body.insertAdjacentHTML('afterbegin', `
-        <div id="${EMPTY_HIDDEN_CHATS_LIST_PLUG_ID}" class="_3Iwj9">
-            <div class="_2t_t3">
+        <div id="${EMPTY_HIDDEN_CHATS_LIST_PLUG_ID}" class="cm280p3y p357zi0d ggj6brxn f8m0rgwh gndfcl4n ac2vgrno ppled2lx gq7nj7y3 td5bf8pq eynyaxvo l3k7h4x6 hp667wtd qfejxiq4">
+            <div class="p357zi0d tvf2evcx oq44ahr5 lb5m6g5c gndfcl4n ac2vgrno f6essjlf j19d4s4u pcx1034m r2u2pyhj myzbk5ck f2yrvmhs g9p5wyxn i0tg5vk9 aoogvgrq o2zu3hjb">
                <span data-testid="empty-archived" data-icon="empty-archived" class="">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 90 90" width="90" height="90">
                      <path fill="currentColor" d="M71.109 24.113l-4.288-5.222c-.594-.934-1.825-1.528-3.396-1.528h-36.85a5.083 5.083 0 0 0-3.693 1.528l-3.991 5.222c-.934 1.231-1.528 2.462-1.528 3.991v38.377a6.16 6.16 0 0 0 6.156 6.156h42.962a6.16 6.16 0 0 0 6.156-6.156V28.104c0-1.529-.595-2.76-1.528-3.991zM45.042 61.896L28.146 45h10.741v-6.156h12.269V45h10.741L45.042 61.896zM23.859 23.519l2.462-3.057H63.17l2.759 3.057h-42.07z"></path>
