86fe7e364b8b933d696f2bce2086c5a397f5882a
Viacheslav Butorin <sloker.new@gmail.com>
2022-06-24 17:13:36 +0500

    Add icon to snooze chat

--- src/features/extension-can/display-zen-mode-ui/construct-zen-mode-ui/construct-hide-contact-item.ts
+++ src/features/extension-can/display-zen-mode-ui/construct-zen-mode-ui/construct-hide-contact-item.ts
@@ -6,8 +6,13 @@ import { set_el_style } from "../../../../../utility-belt/helpers/dom/set-el-sty
 import { construct_hide_popup_area } from "../../../user-can/hide-chat-form-popup/construct-chat-popup";
 
 export function construct_Hide_contact_ctx_menu_item(): HTMLLIElement {
+  const iconUrl = browser.runtime.getURL('assets/whatsapp/hide-snooze-chat.png');
+  const img = DOM.create_el({
+    tag: "img",
+    attributes: {src: iconUrl, id: "WA_contactCtxMenuItem_hide_id"}
+  });
   const menuItemEl = constructFakeCtxMenuItem(
-    [browser.i18n.getMessage("WA_contactCtxMenuItem_hide")],
+    [img, browser.i18n.getMessage("WA_contactCtxMenuItem_hide")],
     hidePopup
   );
   menuItemEl.id = Selectors.ZM_HIDE_CONTACT_CTX_MENU_ITEM.substring(1);
